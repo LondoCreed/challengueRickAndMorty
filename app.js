01435c31eb56f423aa97e0e7f3dd17edc7abac91
Carlos Rene Perez G <d.londocreed@gmail.com>
2024-09-04 20:26:54 -0600

    ajustes js

--- app.js
+++ app.js
@@ -1,211 +1,194 @@
-const { createApp, ref, computed, onMounted, watch } = Vue
 
-const app = createApp({
-    setup() {
-        const characters = ref([])
-        const searchText = ref('')
-        const filterAlive = ref(false)
-        const totalCharacters = ref(0)
-        const currentPage = ref('home')
-        const selectedCharacter = ref(null)
-        const favorites = ref([])
-        const currentPageNumber = ref(1)
-        const totalPages = ref(0)
-        const episodes = ref([])
-        const currentEpisodePage = ref(1)
-        const totalEpisodePages = ref(0)
+//creamos app o instancia vue 
+const app = Vue.createApp({
 
-        // Nuevas variables para estadísticas
-        const topLocations = ref([])
-        const statusCount = ref({})
-        const speciesCount = ref({})
-        const genderCount = ref({})
+    //definimos la data del componente, reactivos  
+    data() {
+        return {
+            characters: [], //Espacio para lospersonajes obtenidos de la data(API)
+            searchText: '', //Texto de busqueda
+            filterAlive: false, //Filtro vivos
+            totalCharacters: 0, //Total de personales
+            currentPage: 'home', //Pagina actual de la app
+            selectedCharacter: null, //personaje tarjeteado para motrar details
+            favorites: [], //Espacio para localStorage
+            currentPageNumber: 1, //Paginacion personajes
+            totalPages: 0, //Paginas totales paginacion
+            episodes: [], //Espacio para episodios obtenidos de la data(API)
+            currentEpisodePage: 1, //Paginacion episodios
+            totalEpisodePages: 0, //Paginas totales de paginacion EP
+            topLocations: [],   //Espacio para ubicaciones comunes
+            statusCount: {},    //Contador de estados
+            speciesCount: {},   //Contador de especies
+            genderCount: {}     //Contador de generos
+        }
+    },
 
-        const fetchCharacters = async (page = 1) => {
+    //definimos las propiedades computadas
+    computed: {
+        //filtramos los personajes segun el textbox y el chek de estado vivo
+        filteredCharacters() {
+            return this.characters.filter(character => {
+                const matchesSearch = character.name.toLowerCase().includes(this.searchText.toLowerCase())
+                const matchesFilter = !this.filterAlive || character.status === 'Alive'
+                return matchesSearch && matchesFilter
+            })
+        },
+        //Rango de paginas para personajes
+        paginationRange() {
+            const range = []
+            for (let i = Math.max(1, this.currentPageNumber - 2); i <= Math.min(this.totalPages, this.currentPageNumber + 2); i++) {
+                range.push(i)
+            }
+            return range
+        },
+        //Rango de paginas para episodios
+        episodePaginationRange() {
+            const range = []
+            for (let i = Math.max(1, this.currentEpisodePage - 2); i <= Math.min(this.totalEpisodePages, this.currentEpisodePage + 2); i++) {
+                range.push(i)
+            }
+            return range
+        }
+    },
+
+    //definimos los metodos
+    methods: {
+        //Obtenemos personajes de la API y actualiza elementos data de la app
+        async fetchCharacters(page = 1) {
             try {
                 const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
                 const data = await response.json()
-                characters.value = data.results
-                totalCharacters.value = data.info.count
-                totalPages.value = data.info.pages
-                currentPageNumber.value = page
-                calculateStats() // Llamada a la nueva función de estadísticas
+                this.characters = data.results
+                this.totalCharacters = data.info.count
+                this.totalPages = data.info.pages
+                this.currentPageNumber = page
+                this.calculateStats()
             } catch (error) {
                 console.error('Error fetching characters:', error)
             }
-        }
-
-        const loadEpisodes = async (page = 1) => {
-            console.log('Loading episodes for page:', page);
+        },
+        //obtenemos episodios de la API y actualiza elementos data de la app
+        async loadEpisodes(page = 1) {
             try {
-                const response = await fetch(`https://rickandmortyapi.com/api/episode?page=${page}`);
-                const data = await response.json();
-                episodes.value = data.results;
-                totalEpisodePages.value = data.info.pages;
-                currentEpisodePage.value = page;
+                const response = await fetch(`https://rickandmortyapi.com/api/episode?page=${page}`)
+                const data = await response.json()
+                this.episodes = data.results
+                this.totalEpisodePages = data.info.pages
+                this.currentEpisodePage = page
             } catch (error) {
-                console.error('Error fetching episodes:', error);
+                console.error('Error fetching episodes:', error)
             }
-        }
+        },
+        //Calculamos estadisticas
+        calculateStats() {
+            this.statusCount = this.characters.reduce((acc, char) => {
+                acc[char.status] = (acc[char.status] || 0) + 1
+                return acc
+            }, {})
 
-        // Nueva función para calcular estadísticas
-        const calculateStats = () => {
-            statusCount.value = characters.value.reduce((acc, char) => {
-                acc[char.status] = (acc[char.status] || 0) + 1;
-                return acc;
-            }, {});
+            this.speciesCount = this.characters.reduce((acc, char) => {
+                acc[char.species] = (acc[char.species] || 0) + 1
+                return acc
+            }, {})
 
-            speciesCount.value = characters.value.reduce((acc, char) => {
-                acc[char.species] = (acc[char.species] || 0) + 1;
-                return acc;
-            }, {});
-
-            const locationCount = characters.value.reduce((acc, char) => {
-                acc[char.location.name] = (acc[char.location.name] || 0) + 1;
-                return acc;
-            }, {});
-            topLocations.value = Object.entries(locationCount)
+            const locationCount = this.characters.reduce((acc, char) => {
+                acc[char.location.name] = (acc[char.location.name] || 0) + 1
+                return acc
+            }, {})
+            this.topLocations = Object.entries(locationCount)
                 .sort((a, b) => b[1] - a[1])
                 .slice(0, 5)
-                .map(([name, count]) => ({ name, count }));
+                .map(([name, count]) => ({ name, count }))
 
-            genderCount.value = characters.value.reduce((acc, char) => {
-                acc[char.gender] = (acc[char.gender] || 0) + 1;
-                return acc;
-            }, {});
-        }
-
-        onMounted(() => {
-            const urlParams = new URLSearchParams(window.location.search)
-            const page = urlParams.get('page')
-
-            if (page === 'episodes') {
-                currentPage.value = 'episodes'
-                loadEpisodes(1)
+            this.genderCount = this.characters.reduce((acc, char) => {
+                acc[char.gender] = (acc[char.gender] || 0) + 1
+                return acc
+            }, {})
+        },
+        //Realizamos el cambio de pagina a details tomando en cuenta el personaje seleccionado
+        showDetails(character) {
+            this.selectedCharacter = character
+            this.currentPage = 'details'
+        },
+        //Verifica si un personaje esta en favorito
+        isFavorite(character) {
+            return this.favorites.some(fav => fav.id === character.id)
+        },
+        //Alternamos el estado de favoritos del personaje
+        toggleFavorite(character) {
+            if (this.isFavorite(character)) {
+                this.removeFromFavorites(character)
             } else {
-                fetchCharacters()
+                this.addToFavorites(character)
             }
-
-            const storedFavorites = localStorage.getItem('favorites')
-            if (storedFavorites) {
-                favorites.value = JSON.parse(storedFavorites)
+        },
+        //Añadimos personaje a favoritos
+        addToFavorites(character) {
+            if (!this.isFavorite(character)) {
+                this.favorites.push(character)
+                this.saveFavorites()
             }
-        })
+        },
 
-        watch(() => currentPage.value, (newValue) => {
-            console.log('Current Page:', newValue);
-            if (newValue === 'episodes') {
-                loadEpisodes(1);
-            }
-        })
+        //Eliminamos personaje de favoritos
+        removeFromFavorites(character) {
+            this.favorites = this.favorites.filter(fav => fav.id !== character.id)
+            this.saveFavorites()
+        },
 
-        const filteredCharacters = computed(() => {
-            return characters.value.filter(character => {
-                const matchesSearch = character.name.toLowerCase().includes(searchText.value.toLowerCase())
-                const matchesFilter = !filterAlive.value || character.status === 'Alive'
-                return matchesSearch && matchesFilter
-            })
-        })
+        //Guardamos favoritos en local storage
+        saveFavorites() {
+            localStorage.setItem('favorites', JSON.stringify(this.favorites))
+        },
 
-        const showDetails = (character) => {
-            selectedCharacter.value = character
-            currentPage.value = 'details'
-        }
-
-        const isFavorite = (character) => {
-            return favorites.value.some(fav => fav.id === character.id)
-        }
-
-        const toggleFavorite = (character) => {
-            if (isFavorite(character)) {
-                removeFromFavorites(character)
-            } else {
-                addToFavorites(character)
-            }
-        }
-
-        const addToFavorites = (character) => {
-            if (!isFavorite(character)) {
-                favorites.value.push(character)
-                saveFavorites()
-            }
-        }
-
-        const removeFromFavorites = (character) => {
-            favorites.value = favorites.value.filter(fav => fav.id !== character.id)
-            saveFavorites()
-        }
-
-        const saveFavorites = () => {
-            localStorage.setItem('favorites', JSON.stringify(favorites.value))
-        }
-
-        const openFavoritesModal = () => {
+        //Para abrir el modal de favoritos con bootstrap
+        openFavoritesModal() {
             const modal = new bootstrap.Modal(document.getElementById('favoritesModal'))
             modal.show()
-        }
+        },
+        //Cambiamos de pagina en personajes
+        goToPage(page) {
+            if (page >= 1 && page <= this.totalPages) {
+                this.fetchCharacters(page)
+            }
+        },
 
-        const goToPage = (page) => {
-            if (page >= 1 && page <= totalPages.value) {
-                fetchCharacters(page)
+        //Cambiamos de pagina en episodios
+        goToEpisodePage(page) {
+            if (page >= 1 && page <= this.totalEpisodePages) {
+                this.loadEpisodes(page)
             }
         }
+    },
 
-        const goToEpisodePage = (page) => {
-            if (page >= 1 && page <= totalEpisodePages.value) {
-                loadEpisodes(page)
-            }
+    //montamos componentes, definimos que cargara dependiendo si estamos en episodios o favoritos
+    mounted() {
+        const urlParams = new URLSearchParams(window.location.search)
+        const page = urlParams.get('page')
+
+        if (page === 'episodes') {
+            this.currentPage = 'episodes'
+            this.loadEpisodes(1)
+        } else {
+            this.fetchCharacters()
         }
 
-        const paginationRange = computed(() => {
-            const range = []
-            for (let i = Math.max(1, currentPageNumber.value - 2); i <= Math.min(totalPages.value, currentPageNumber.value + 2); i++) {
-                range.push(i)
+        const storedFavorites = localStorage.getItem('favorites')
+        if (storedFavorites) {
+            this.favorites = JSON.parse(storedFavorites)
+        }
+    },
+    //Observador de cambios de pagina
+    watch: {
+        currentPage(newValue) {
+            console.log('Current Page:', newValue)
+            if (newValue === 'episodes') {
+                this.loadEpisodes(1)
             }
-            return range
-        })
-
-        const episodePaginationRange = computed(() => {
-            const range = []
-            for (let i = Math.max(1, currentEpisodePage.value - 2); i <= Math.min(totalEpisodePages.value, currentEpisodePage.value + 2); i++) {
-                range.push(i)
-            }
-            return range
-        })
-
-        return {
-            characters,
-            searchText,
-            filterAlive,
-            totalCharacters,
-            currentPage,
-            selectedCharacter,
-            favorites,
-            currentPageNumber,
-            totalPages,
-            filteredCharacters,
-            showDetails,
-            addToFavorites,
-            removeFromFavorites,
-            openFavoritesModal,
-            goToPage,
-            paginationRange,
-            episodes,
-            currentEpisodePage,
-            totalEpisodePages,
-            loadEpisodes,
-            goToEpisodePage,
-            episodePaginationRange,
-            // Nuevas propiedades para estadísticas
-            topLocations,
-            statusCount,
-            speciesCount,
-            genderCount,
-            // Propiedades y métodos para favoritos
-            isFavorite,
-            toggleFavorite
         }
     }
 })
 
+//Inicializamos la app en el contenedor especificado en el html
 app.mount('#app')
